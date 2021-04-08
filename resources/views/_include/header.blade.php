d84715c11034d999e685c9dcb1cc1cb0f94d383d
Anup Shrestha <anupshresth9@gmail.com>
2021-04-08 21:37:14 +0545

    notification updated

--- resources/views/_include/header.blade.php
+++ resources/views/_include/header.blade.php
@@ -10,25 +10,31 @@
 
 
                 @if(Auth::user())
+                <?php $notifications = Auth::user()->userNotification()  ?>
                 <li class="nav-item d-flex align-items-center">
                     <div class="dropdown d-flex align-items-center">
                         <button class="btn btn-transparent  dropdown-toggle shadow-none d-flex align-items-center "
-                            type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            style="position: relative;" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <div class="bg-primary text-white"
+                                style="font-size: .8rem; position: absolute;top: -10px; right:10px; z-index: inherit;  clip-path: circle(); padding:5px">
+                                <span>{{ $notifications->count() }}</span>
+                            </div>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-bell" viewBox="0 0 16 16">
                                 <path
                                     d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
                             </svg>
                         </button>
-                        <?php $notifications = Auth::user()->userNotification()  ?>
                         <div class="dropdown-menu dropdown-menu-end  p-0 notification"
                             aria-labelledby="dropdownMenuButton1" style="width: 300px;margin-top:5px; max-height: 500px; overflow-y: scroll ;ms-overflow-style: none;  /* IE and Edge */
   scrollbar-width: none;">
-                            <h6 class="bg-success text-white p-2">Your Notifications</h6>
+                            <h6 class="bg-success text-white p-2 m-0" style="position: sticky; top: 0">Your
+                                Notifications</h6>
                             @forelse ($notifications as $notification)
                             {{-- <div @if($notification->status == 0) style="background-color:#d4d2cd" @endif> --}}
 
-                            <hr>
+                            <hr class="my-0">
                             <p class="text-muted w-100  p-2">
                                 <strong> {{$notification->notificationFrom->name}} {{$notification->message}}
                                 </strong>
@@ -36,7 +42,7 @@
                                     ({{$notification->notifiable->title}}) </a>
                             </p>
 
-                            <hr>
+
                             {{-- </div> --}}
 
                             @empty
@@ -54,8 +60,8 @@
                     <div class="dropdown">
                         <button class="btn btn-transparent dropdown-toggle shadow-none d-flex align-items-center "
                             type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="{{ Auth::user()->profile_photo_url }}" alt="" width="30"
-                                class="img-fluid rounded-circle mx-2">
+                            <img src="{{ Auth::user()->profile_photo_url }}" alt="" width="45"
+                                class="img-fluid rounded-circle mx-2 border border-primary border-2 ">
                             <span>
                                 {{ Auth::user()->name }}</span>
                         </button>
