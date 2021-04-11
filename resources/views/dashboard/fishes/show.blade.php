1a8ad4258c7668c201a443aef640ac40babc16f7
Suraj <suraznep@gmail.com>
2021-04-11 09:56:40 +0545

    Solved Fishes SHow FIle

--- resources/views/dashboard/fishes/show.blade.php
+++ resources/views/dashboard/fishes/show.blade.php
@@ -14,41 +14,38 @@
 
     <div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
-
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-10">
-              @if(Session::has('success'))
-              <p style="color: green"> {{Session::get('success')}}</p>
-              @endif
-                <h1 class="pb-4 text-2xl capitalize text-gray-700 ">{{ $fish->name }}</h1>
-                <hr class="border-2 w-100 mb-5 ">
-                <div class="text-sm my-4">
-                    <p>
-                        Fish Type : {{$fishCategory->title}}
-                    </p>
-                <hr class="border-2 w-100 mb-5 ">
-
-                    <p>
-                        Created At : {{ $fish->created_at }}
-                    </p>
-
-                
-                 
-                    
-               
-                  
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg ">
+                @if(Session::has('success'))
+                <p style="color: green"> {{Session::get('success')}}</p>
+                @endif
+                <div class="px-4 py-5 sm:px-6">
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                        Fish Information
+                    </h3>
+                </div>
+                <div class="border-t border-gray-200">
+                    <dl>
+                        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium text-gray-500">
+                                Fish name
+                            </dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {{ $fish->name }}
+                            </dd>
+                        </div>
+                        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium text-gray-500">
+                                Fish Type
+                            </dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                              {{$fishCategory->title}}
+                            </dd>
+                        </div>
+                    </dl>
                 </div>
 
-             
-                <hr class="border-2 w-100 mb-5 ">
-                <h1 class="pb-4 text-2xl capitalize text-gray-700 ">Fish Compactibilities</h1>
 
-
-               
-             
-
-
-
-              
+                <h1 class="pb-4 text-2xl capitalize text-gray-700 px-4 pt-5 ">Fish Compactibilities</h1>
 
                 <form method="POST" action="{{route('fish.savecompactibility',$fish->id)}}" class="px-4">
 
@@ -107,7 +104,6 @@
                     </div>
 
                 </form>
-
 
             </div>
 
