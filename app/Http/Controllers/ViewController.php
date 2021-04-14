910d4b1e2829c524727bfb9e004a7042c0c370c5
Anup Shrestha <anupshresth9@gmail.com>
2021-04-14 10:00:37 +0545

    removed unnecessary code

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -298,18 +298,6 @@ class ViewController extends Controller {
     public function fish_check(Category $category, Request $request) {
 
         $fishes = Fish::whereIn('id', $request->fishes)->get();
-
-        $compactibility = [];
-
-        foreach ($fishes as $key => $fish) {
-            $compactibility[$key] = [];
-            foreach ($fishes as $selectedFish) {
-                $compactibility[$key] = [...$compactibility[$key], $fish->compactibilities()->where('compactible_fish_id', $selectedFish->id)->first()];
-            }
-        }
-
-        // dd($compactibility[0][0]->compactibility);
-
         return view('frontend.fish-compactibility', compact('fishes', 'category'));
 
     }
