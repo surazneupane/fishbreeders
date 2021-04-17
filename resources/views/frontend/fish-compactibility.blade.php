22d8aff199addfefc0bc41008f54c72ef524e3e3
Anup Shrestha <anupshresth9@gmail.com>
2021-04-17 13:06:12 +0545

    fixed some issues

--- resources/views/frontend/fish-compactibility.blade.php
+++ resources/views/frontend/fish-compactibility.blade.php
@@ -20,7 +20,7 @@
 
 
     <div class="row ">
-        <div class="col-lg-6 p-2 mx-auto">
+        <div class="col-lg-6 p-2 mx-auto ">
 
             <h3>
                 FreshWater Fish
@@ -45,6 +45,143 @@
             @isset($category)
 
             @if($category->id == 1)
+            @isset($fishes)
+
+            <div class="my-5 w-100">
+                <h4>Fish Compatibility Result</h4>
+                <div>
+                    <h6>Legends</h6>
+                </div>
+                <div class="d-flex justify-content-between align-items-center flex-wrap">
+
+                    <div class="d-flex align-items-center">
+                        <div style="width:20px; height:20px; border: 1px solid #000 " class="bg-success m-1">
+                        </div>
+                        <small>
+                            Compatible
+                        </small>
+                    </div>
+
+
+                    <div class="d-flex align-items-center">
+                        <div style="width:20px; height:20px; border: 1px solid #000 " class="bg-warning m-1">
+                        </div>
+                        <small>
+                            Average
+                        </small>
+                    </div>
+
+                    <div class="d-flex align-items-center">
+                        <div style="width:20px; height:20px; border: 1px solid #000 " class="bg-danger m-1">
+                        </div>
+                        <small>
+                            Not Compatible
+                        </small>
+                    </div>
+
+                    <div class="d-flex align-items-center">
+                        <div style="width:20px; height:20px; border: 1px solid #000 " class="bg-light m-1">
+                        </div>
+                        <small>
+                            No Result
+                        </small>
+                    </div>
+                </div>
+                <div class="table-responsive">
+
+
+                    <table class="table table-striped  table-hover " style="style:100%;overflow-x:scroll">
+                        <thead>
+                            <tr>
+                                <th scope="col" style="position: sticky; left:0;  background: #fff !important;"></th>
+
+                                @foreach ($fishes as $fish )
+                                <th scope="col">
+                                    <small>
+                                        {{ $fish->name }}
+
+                                    </small> </th>
+                                @endforeach
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($fishes as $fish )
+                            <tr>
+                                <th scope="row " style="position: sticky; left:0;  background: #fff !important; ">
+                                    <small>
+                                        {{ $fish->name }}
+                                    </small>
+                                </th>
+                                @foreach ($fishes as $compactibleFish )
+                                <td scope="col">
+
+                                    @if($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 1)->first())
+
+                                    <div style="width:40px; height:40px; border: 1px solid #e6e6e6 " class="bg-success">
+                                    </div>
+
+                                    @elseif($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 2)->first())
+
+                                    <div style="width:40px; height:40px; border: 1px solid #e6e6e6 " class="bg-warning">
+                                    </div>
+                                    @elseif($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 3)->first())
+
+                                    <div style="width:40px; height:40px; border: 1px solid #e6e6e6 " class="bg-danger">
+                                    </div>
+
+                                    @else
+                                    <div style="width:40px; height:40px; border: 1px solid #e6e6e6 " class="bg-light">
+                                    </div>
+
+                                    @endif
+
+
+                                </td>
+                                @endforeach
+                            </tr>
+                            @endforeach
+                        </tbody>
+
+
+                    </table>
+                </div>
+
+
+            </div>
+            @endisset
+            @endif
+            @endisset
+
+
+        </div>
+        <div class="col-lg-6 p-2 mx-auto">
+
+            <h3>
+                SaltWater Fish
+            </h3>
+            <form action="{{ route('fish.check',2 ) }}" method="POST">
+                @csrf
+
+                <div class="mb-3">
+                    <label for="saltfish" class="form-label">Select Fishes</label>
+                    <select required name="fishes[]" id="saltfish" class="form-control" multiple>
+                        <option></option>
+                        @foreach (\App\Models\Category::find(2)->fishes as $fish)
+                        <option value="{{ $fish->id }}">{{ $fish->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-outline-primary px-4 py-1 rounded-pill" type="submit">
+                        Check
+                    </button>
+                </div>
+
+            </form>
+
+            @isset($category)
+
+            @if($category->id == 2)
             @isset($fishes)
 
             <div class="my-5">
@@ -87,176 +224,70 @@
                         </small>
                     </div>
                 </div>
-                <table class="table table-striped">
-                    <tr>
-                        <th scope="col"></th>
-                        @foreach ($fishes as $fish )
-                        <th scope="col">
-                            {{ $fish->name }}
-                        </th>
-                        @endforeach
-                    </tr>
-                    @foreach ($fishes as $fish )
-                    <tr>
-                        <th scope="row">
-                            {{ $fish->name }}
-                        </th>
-                        @foreach ($fishes as $compactibleFish )
-                        <td scope="col">
-
-                            @if($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 1)->first())
-
-                            <div style="width:40px; height:40px; border: 1px solid #e6e6e6 " class="bg-success">
-                            </div>
-
-                            @elseif($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 2)->first())
-
-                            <div style="width:40px; height:40px; border: 1px solid #e6e6e6 " class="bg-warning">
-                            </div>
-                            @elseif($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 3)->first())
-
-                            <div style="width:40px; height:40px; border: 1px solid #e6e6e6 " class="bg-danger">
-                            </div>
-
-                            @else
-                            <div style="width:40px; height:40px; border: 1px solid #e6e6e6 " class="bg-light">
-                            </div>
-
-                            @endif
+                <div class="table-responsive">
 
 
-                        </td>
-                        @endforeach
-                    </tr>
-                    @endforeach
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col" style="position: sticky; left:0;  background: #fff !important; "></th>
 
-                </table>
-            </div>
-            @endisset
-            @endif
-            @endisset
+                                @foreach ($fishes as $fish )
+                                <th scope="col">
+                                    <small>
+                                        {{ $fish->name }}
+                                    </small>
+
+                                </th>
+                                @endforeach
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($fishes as $fish )
+                            <tr>
+                                <th scope="row" style="position: sticky; left:0;  background: #fff !important; ">
+
+                                    <small>
+                                        {{ $fish->name }}
+                                    </small>
+
+                                </th>
+                                @foreach ($fishes as $compactibleFish )
+                                <td scope="col">
+
+                                    @if($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 1)->first())
+
+                                    <div style="width:40px; height:40px; border: 1px solid #e6e6e6" class="bg-success">
+
+                                    </div>
+
+                                    @elseif($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 2)->first())
+
+                                    <div style="width:40px; height:40px; border: 1px solid #e6e6e6" class="bg-warning">
+
+                                    </div>
+                                    @elseif($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 3)->first())
+
+                                    <div style="width:40px; height:40px; border: 1px solid #e6e6e6" class="bg-danger">
+
+                                    </div>
+
+                                    @else
+                                    <div style="width:40px; height:40px; border: 1px solid #e6e6e6" class="bg-light">
+
+                                    </div>
+
+                                    @endif
 
 
-        </div>
-        <div class="col-lg-6 p-2 mx-auto">
+                                </td>
+                                @endforeach
+                            </tr>
+                            @endforeach
+                        </tbody>
 
-            <h3>
-                SaltWater Fish
-            </h3>
-            <form action="{{ route('fish.check',2 ) }}" method="POST">
-                @csrf
-
-                <div class="mb-3">
-                    <label for="saltfish" class="form-label">Select Fishes</label>
-                    <select required name="fishes[]" id="saltfish" class="form-control" multiple>
-                        <option></option>
-                        @foreach (\App\Models\Category::find(2)->fishes as $fish)
-                        <option value="{{ $fish->id }}">{{ $fish->name }}</option>
-                        @endforeach
-                    </select>
+                    </table>
                 </div>
-                <div class="mb-3">
-                    <button class="btn btn-outline-primary px-4 py-1 rounded-pill" type="submit">
-                        Check
-                    </button>
-                </div>
-
-            </form>
-
-            @isset($category)
-
-            @if($category->id == 2)
-            @isset($fishes)
-
-            <div class="my-5">
-                <h4>Fish Compatibility Result</h4>
-                        <div>
-                            <h6>Legends</h6>
-                        </div>
-                        <div class="d-flex justify-content-between align-items-center flex-wrap">
-
-                            <div class="d-flex align-items-center">
-                                <div style="width:20px; height:20px; border: 1px solid #000 " class="bg-success m-1">
-                                </div>
-                                <small>
-                                    Compatible
-                                </small>
-                            </div>
-
-
-                            <div class="d-flex align-items-center">
-                                <div style="width:20px; height:20px; border: 1px solid #000 " class="bg-warning m-1">
-                                </div>
-                                <small>
-                                    Average
-                                </small>
-                            </div>
-
-                            <div class="d-flex align-items-center">
-                                <div style="width:20px; height:20px; border: 1px solid #000 " class="bg-danger m-1">
-                                </div>
-                                <small>
-                                    Not Compatible
-                                </small>
-                            </div>
-
-                            <div class="d-flex align-items-center">
-                                <div style="width:20px; height:20px; border: 1px solid #000 " class="bg-light m-1">
-                                </div>
-                                <small>
-                                    No Result
-                                </small>
-                            </div>
-                        </div>
-
-                <table class="table table-striped">
-                    <tr>
-                        <th scope="col"></th>
-                        @foreach ($fishes as $fish )
-                        <th scope="col">
-                            {{ $fish->name }}
-                        </th>
-                        @endforeach
-                    </tr>
-                    @foreach ($fishes as $fish )
-                    <tr>
-                        <th scope="row">
-                            {{ $fish->name }}
-                        </th>
-                        @foreach ($fishes as $compactibleFish )
-                        <td scope="col">
-
-                            @if($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 1)->first())
-
-                            <div style="width:40px; height:40px; border: 1px solid #e6e6e6" class="bg-success">
-
-                            </div>
-
-                            @elseif($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 2)->first())
-
-                            <div style="width:40px; height:40px; border: 1px solid #e6e6e6" class="bg-warning">
-
-                            </div>
-                            @elseif($compactibleFish->compactibilities()->where('compactible_fish_id', $fish->id)->where('compactibility_id', 3)->first())
-
-                            <div style="width:40px; height:40px; border: 1px solid #e6e6e6" class="bg-danger">
-
-                            </div>
-
-                            @else
-                            <div style="width:40px; height:40px; border: 1px solid #e6e6e6" class="bg-light">
-
-                            </div>
-
-                            @endif
-
-
-                        </td>
-                        @endforeach
-                    </tr>
-                    @endforeach
-
-                </table>
             </div>
             @endisset
             @endif
